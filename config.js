3b8b51e72ed7f44b2afbec2f4e89898af2075024
aryjuanda <108046927+aryjuanda@users.noreply.github.com>
2022-06-24 19:46:54 +0700

    Update config.js

--- config.js
+++ config.js
@@ -40,8 +40,8 @@ process.env['NODE_TLS_REJECT_UNAUTHORIZED'] = 0
 const spack = fs.readFileSync("lib/exif.json")
 const stickerpack = JSON.parse(spack)
 if (stickerpack.spackname == '') {
-  var sticker_name = '@penta_bot'
-  var sticker_author = 'penta'
+  var sticker_name = '@Ganz_bot'
+  var sticker_author = 'Ary'
 } else {
   var sticker_name = stickerpack.spackname
   var sticker_author = stickerpack.sauthor
@@ -58,7 +58,7 @@ fs.watchFile(file_exif, () => {
 // Sticker WM
 global.packname = sticker_name
 global.author = sticker_author
-global.wm = '© Gempy'
+global.wm = '© Ary'
 
 global.multiplier = 1000 // The higher, The harder levelup
 
